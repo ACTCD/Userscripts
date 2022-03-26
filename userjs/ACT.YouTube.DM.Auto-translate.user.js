1880ba9195b644d828861fe6751fda6933a872bd
ACTCD <101378590+ACTCD@users.noreply.github.com>
2022-03-27 07:49:47 +0800

    Update ACT.YouTube.DM.Auto-translate.user.js

--- userjs/ACT.YouTube.DM.Auto-translate.user.js
+++ userjs/ACT.YouTube.DM.Auto-translate.user.js
@@ -4,7 +4,7 @@
 // @description        Automatically translate any non-specified language Subtitles/CC.
 // @description:zh-CN  自动翻译任何非指定语言字幕。
 // @author             ACTCD
-// @version            20220327.2
+// @version            20220327.3
 // @license            GPL-3.0-or-later
 // @namespace          ACTCD/Userscripts
 // @supportURL         https://github.com/ACTCD/Userscripts#contact
@@ -54,7 +54,7 @@
         document.head.appendChild(script);
     } else {
         new MutationObserver((mutationList, observer) => {
-            document.head && observer.disconnect() || document.head.append(script);
+            document.head && (observer.disconnect() || document.head.append(script));
         }).observe(document, { subtree: true, childList: true });
     }
 
