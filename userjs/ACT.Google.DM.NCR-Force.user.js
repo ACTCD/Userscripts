cc38ce2b61c58e6a81b845bed5ecd10da7e3a0e9
ACTCD <101378590+ACTCD@users.noreply.github.com>
2022-04-03 21:13:39 +0800

    Create ACT.Google.DM.NCR-Force.user.js

---
+++ userjs/ACT.Google.DM.NCR-Force.user.js
@@ -0,0 +1,13 @@
+// ==UserScript==
+// @name               ACT.Google.DM.NCR-Force
+// @author             ACTCD
+// @version            20220401.1
+// @license            GPL-3.0-or-later
+// @namespace          ACTCD/Userscripts
+// @supportURL         https://github.com/ACTCD/Userscripts#contact
+// @homepageURL        https://github.com/ACTCD/Userscripts
+// @updateURL          https://raw.githubusercontent.com/ACTCD/Userscripts/main/userjs/ACT.Google.DM.NCR.user.js
+// @downloadURL        https://raw.githubusercontent.com/ACTCD/Userscripts/main/userjs/ACT.Google.DM.NCR.user.js
+// @grant              none
+// @run-at             document-start
+// ==/UserScript==
