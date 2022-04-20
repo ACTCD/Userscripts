51241de0d865771fb7b9b6fda0b51c2f78de3f91
ACTCD <101378590+ACTCD@users.noreply.github.com>
2022-04-20 09:03:04 +0800

    Update ACT.Google.DM.NCR-Force.user.js

--- userjs/ACT.Google.DM.NCR-Force.user.js
+++ userjs/ACT.Google.DM.NCR-Force.user.js
@@ -1,5 +1,5 @@
 // ==UserScript==
-// @name               ACT.Google.DM.NCR.SHIFT
+// @name               ACT.Google.DM.NCR-Force
 // @author             ACTCD
 // @version            20220401.1
 // @license            GPL-3.0-or-later
@@ -11,3 +11,6 @@
 // @grant              none
 // @run-at             document-start
 // ==/UserScript==
+
+// 此脚本已合入“ACT.Google.DM.NCR.user.js”，此文件仅供自动更新迁移使用。不久后此文件将会被移除。
+// This script has been merged into "ACT.Google.DM.NCR.user.js", this file is only used for automatic update migration. This file will be removed shortly.
