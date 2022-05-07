4ff794929281801515ce1f1cb7857d0be132c43e
ACTCD <101378590+ACTCD@users.noreply.github.com>
2022-05-07 17:20:38 +0800

    Update ACT.Google.MO.Auto-Seemore.user.js

--- userjs/ACT.Google.MO.Auto-Seemore.user.js
+++ userjs/ACT.Google.MO.Auto-Seemore.user.js
@@ -4,15 +4,14 @@
 // @description        Mobile google search See more automatically.
 // @description:zh-CN  移动网站谷歌搜索自动查看更多。
 // @author             ACTCD
-// @version            20220326.1
+// @version            20220507.1
 // @license            GPL-3.0-or-later
 // @namespace          ACTCD/Userscripts
 // @supportURL         https://github.com/ACTCD/Userscripts#contact
 // @homepageURL        https://github.com/ACTCD/Userscripts
 // @updateURL          https://raw.githubusercontent.com/ACTCD/Userscripts/main/userjs/ACT.Google.MO.Auto-Seemore.user.js
 // @downloadURL        https://raw.githubusercontent.com/ACTCD/Userscripts/main/userjs/ACT.Google.MO.Auto-Seemore.user.js
-// @match              *://*.google.com/*
-// @match              *://*.google.com.hk/*
+// @match              https://www.google.com/search*
 // @grant              none
 // @run-at             document-start
 // ==/UserScript==
@@ -23,7 +22,7 @@
     window.addEventListener('scroll', () => {
         if ((window.pageYOffset + window.innerHeight * 2) < document.body.clientHeight) return;
         let button = document.querySelector("a[aria-label='See more'],a[aria-label='查看更多']");
-        if (button?.style.display != "none") button?.click();
+        if (button?.style.display !== "none") button?.click();
     });
 
 })();
