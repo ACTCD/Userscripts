06d5959ed06ea645bb9ba251d936ce0be1da17d4
ACTCD <101378590+ACTCD@users.noreply.github.com>
2022-08-27 12:32:51 +0800

    Update ACT.Douban.MO.Stay.user.js

--- userjs/ACT.Douban.MO.Stay.user.js
+++ userjs/ACT.Douban.MO.Stay.user.js
@@ -4,10 +4,10 @@
 // @description        Stay in web not app, browsing experience optimization.
 // @description:zh-CN  留在网络而非应用，网站浏览体验优化。
 // @author             ACTCD
-// @version            20220722.1
+// @version            20220827.1
 // @license            GPL-3.0-or-later
 // @namespace          ACTCD/Userscripts
-// @supportURL         https://github.com/ACTCD/Userscripts#contact
+// @supportURL         https://github.com/ACTCD/Userscripts
 // @homepageURL        https://github.com/ACTCD/Userscripts
 // @updateURL          https://raw.githubusercontent.com/ACTCD/Userscripts/main/userjs/ACT.Douban.MO.Stay.user.js
 // @downloadURL        https://raw.githubusercontent.com/ACTCD/Userscripts/main/userjs/ACT.Douban.MO.Stay.user.js
@@ -22,19 +22,8 @@
 
     function cleaner() {
         document.querySelector("#home_guide")?.click(); // Cover App banner
-        document.querySelector(".TalionNav-static")?.remove(); // Top App banner
-        document.querySelector(".page")?.style.setProperty("margin-top", "50px"); // Top Space Fix
-        document.querySelector(".download-app")?.remove(); // Bottom App banner
-        document.querySelector(".open-in-app-fixed-bottom")?.remove(); // Bottom App banner
-        document.body.style.setProperty("padding-bottom", ""); // Bottom Space Fix
         document.querySelectorAll("a[href*='/doubanapp'],a[href*='/to_app']").forEach(e => e.removeAttribute("href")); // APP Jump Links
-        document.querySelectorAll("section").forEach(e => e.querySelector("span")?.innerHTML == "豆瓣榜单" && e.remove());
-        document.querySelectorAll("section h2 a").forEach(e => e.remove()); // 用App看更多
-        document.querySelector("a.score-write")?.remove(); // Embed App banner
-        document.querySelector(".vendor-go-app")?.remove(); // Embed App banner
-        document.querySelector(".subject-banner")?.remove(); // Embed App banner
-        document.querySelectorAll(".center").forEach(e => e.remove()); // AD
-        document.querySelectorAll("div[style='position: relative;margin: 0 -18px']").forEach(e => e.remove()); // AD
+        document.querySelectorAll("section").forEach(e => e.innerHTML.includes('用App打开') && e.remove());
         document.querySelector(".expand")?.click(); // Auto Expand
         document.querySelector(".read-all a")?.click(); // Auto Expand
     }
@@ -49,6 +38,29 @@
         document.addEventListener('DOMContentLoaded', DOMContentLoaded);
     } else {
         DOMContentLoaded();
+    }
+
+    const style = document.createElement('style');
+    style.textContent = `/* Global style */
+.TalionNav-static { display: none !important; } /* Top App banner */
+.page { margin-top: 50px !important; top: 0 !important; } /* Top Space Fix */
+.download-app { display: none !important; } /* Bottom App banner */
+.open-in-app-fixed-bottom { display: none !important; } /* Bottom App banner */
+body { padding-bottom: 0 !important; } /* Bottom Space Fix */
+section h2 a { display: none !important; } /* 用App看更多 */
+a.score-write { display: none !important; } /* Embed App banner */
+.vendor-go-app { display: none !important; } /* Embed App banner */
+.subject-banner { display: none !important; } /* Embed App banner */
+.center { display: none !important; } /* AD */
+div[style='position: relative;margin: 0 -18px'] { display: none !important; } /* AD */
+`;
+
+    if (document.head) {
+        document.head.append(style);
+    } else {
+        new MutationObserver((mutationList, observer) => {
+            document.head && (observer.disconnect(), document.head.append(style));
+        }).observe(document, { subtree: true, childList: true });
     }
 
 })();
