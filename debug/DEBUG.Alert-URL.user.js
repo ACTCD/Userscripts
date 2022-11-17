fbc621fe3db3a6a52c65c16953e8ca9d1d94d958
ACTCD <101378590+ACTCD@users.noreply.github.com>
2022-11-18 06:03:31 +0800

    Create DEBUG.Alert-URL.user.js

---
+++ debug/DEBUG.Alert-URL.user.js
@@ -0,0 +1,15 @@
+// ==UserScript==
+// @name               DEBUG.Alert-URL
+// @description        Demo user script alert URL.
+// @author             ACTCD
+// @version            20221118.1
+// @match              *://*/*
+// @grant              none
+// @inject-into        content
+// @run-at             document-start
+// ==/UserScript==
+
+(function () {
+    'use strict';
+    alert("DEBUG.Alert-URL:\n\n" + location);
+})();
