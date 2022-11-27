65d5cad5d8ddb9693d0a6e3a5dc2c82f9c1b9c0f
ACTCD <101378590+ACTCD@users.noreply.github.com>
2022-11-27 17:59:50 +0800

    Create ACT.Twitter.DM.Anti-loginWall.user.js

---
+++ userjs/ACT.Twitter.DM.Anti-loginWall.user.js
@@ -0,0 +1,57 @@
+// ==UserScript==
+// @name               ACT.Twitter.DM.Anti-loginWall
+// @name:zh-CN         ACT.推特.DM.反登录墙
+// @description        Anti login wall.
+// @description:zh-CN  去除登录墙限制。
+// @author             ACTCD
+// @version            20221127.1
+// @license            GPL-3.0-or-later
+// @namespace          ACTCD/Userscripts
+// @supportURL         https://github.com/ACTCD/Userscripts
+// @homepageURL        https://github.com/ACTCD/Userscripts
+// @updateURL          https://raw.githubusercontent.com/ACTCD/Userscripts/main/userjs/ACT.Twitter.DM.Anti-loginWall.user.js
+// @downloadURL        https://raw.githubusercontent.com/ACTCD/Userscripts/main/userjs/ACT.Twitter.DM.Anti-loginWall.user.js
+// @match              *://*.twitter.com/*
+// @grant              none
+// @inject-into        content
+// @run-at             document-start
+// ==/UserScript==
+
+(function () {
+    'use strict';
+
+    function cleaner() {
+        // document.querySelector("[data-testid='app-bar-close']")?.click();
+        document.querySelector("[href='/login']")?.closest("#layers>div")?.style.setProperty("display", "none"); // Bottom login banner
+        document.querySelector("[href='/signup']")?.closest("#layers>div")?.style.setProperty("display", "none"); // Cover login wall
+    }
+
+    new MutationObserver(cleaner).observe(document, { subtree: true, childList: true });
+
+    function DOMContentLoaded() {
+        cleaner();
+    }
+
+    if (document.readyState === 'loading') {
+        document.addEventListener('DOMContentLoaded', DOMContentLoaded);
+    } else {
+        DOMContentLoaded();
+    }
+
+    const style = document.createElement('style');
+    style.textContent = `/* Global style */
+html { overflow-y: scroll !important; } /* Scroll fix */
+#credential_picker_container { display: none !important; } /* Float Google login */
+/* #layers */
+[data-testid='BottomBar'] { display: none !important; } /* Bottom login banner */
+`;
+
+    if (document.head) {
+        document.head.append(style);
+    } else {
+        new MutationObserver((mutationList, observer) => {
+            document.head && (observer.disconnect(), document.head.append(style));
+        }).observe(document, { subtree: true, childList: true });
+    }
+
+})();
