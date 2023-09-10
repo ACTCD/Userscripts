4b86f413e0d4452a672c0c831eafd6715917d32b
ACTCD <101378590+ACTCD@users.noreply.github.com>
2023-09-10 12:48:08 +0800

    Create DEBUG.Meta-require.user.js

---
+++ debug/DEBUG.Meta-require.user.js
@@ -0,0 +1,16 @@
+// ==UserScript==
+// @name               DEBUG.Meta-require
+// @description        Demo user script alert URL.
+// @author             ACTCD
+// @version            20230910.1
+// @match              *://example.com/*
+// @grant              none
+// @run-at             document-start
+// @require            https://cdnjs.cloudflare.com/ajax/libs/jquery/3.7.0/jquery.min.js
+// @require            https://cdnjs.cloudflare.com/ajax/libs/jquery/3.7.1/jquery.min.js
+// ==/UserScript==
+
+(function () {
+    'use strict';
+    alert(`jQuery: ${window?.jQuery?.fn?.jquery ?? 'unload'}`);
+})();
